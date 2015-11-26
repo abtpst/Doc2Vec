f9b3e90cfad012f503650f7f4344b7152f540913
Abhijit Tomar <abhijit141088@gmail.com>
2015-11-26 12:59:24 -0500

    Update prep.py
    added more documentation

--- prep.py
+++ prep.py
@@ -1,8 +1,3 @@
-'''
-Created on Sep 23, 2015
-
-@author: atomar
-'''
 import pickle
 import json
 from gensim.models.doc2vec import TaggedDocument
@@ -18,25 +13,32 @@ def labelizeReviews(reviewSet,labelType):
         labelized = []
         for index, review in enumerate(reviewSet):
             
+            #We give two lables to each review. The 'LABELED/UNLABELED_INDEX' and the sentiment
             if(labelType == "LABELED"):
                 
                 sentiment = review.pop()
+                
                 labelized.append(TaggedDocument(words=review, tags=['%s_%s'%(labelType, index),sentiment]))
             
             else:
-            
                 labelized.append(TaggedDocument(words=review, tags=['%s_%s'%(labelType, index)]))
               
         return labelized
-   
+
+#Load cleaned up labeled reviews   
 labeled = json.load(open("../../classifier/doc2vec/labeledSentiFFF.json", "r"))
 
+#Load cleaned up unlabeled reviews
 unlabeled = json.load(open("../../classifier/doc2vec/unlabeledFFF.json", "r"))
-   
+
+#Tag the cleaned labeled reviews   
 labeled = labelizeReviews(labeled, 'LABELED')
 
+#Tag the cleaned unlabeled reviews
 unlabeled = labelizeReviews(unlabeled, 'UNLABELED')
-    
+
+#Combine the tagged reviews    
 bagTaggedDocs = labeled + unlabeled
 
+#Save tagged reviews
 pickle.dump(bagTaggedDocs,open("../../classifier/doc2vec/taggedDocs.pickle", "wb"))
